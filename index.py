89c023b2745094424b7cfe4c5502d36e2ac2d99d
rahulvk007 <59737849+rahulvk007@users.noreply.github.com>
2021-11-22 19:15:45 +0530

    new project

--- index.py
+++ index.py
@@ -1,21 +1,30 @@
-from flask import Flask
-from bs4 import BeautifulSoup
-import requests
-import sys
+from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
 
-@app.route('/<x>')
-def home(x):
-    url = 'https://en.wikipedia.org/wiki/' + x
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.title.get_text()
-    x = soup.find_all('p')
-    data = str()
-    for i in x:
-        data = data + i.get_text() + '\n'
-    return  data
+
+host = "localhost"
+user = "root"
+password = "rahul3791"
+db = "elements"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def employees():
+    if request.method == "POST":
+        details = request.form
+        at_no = details['atomic_number']
+        con = pymysql.connect(host=host, user=user, password=password,
+                              db=db, cursorclass=pymysql.cursors.DictCursor)
+        cur = con.cursor()
+        cur.execute(
+            "SELECT name,atomic_number,atomic_weight,symbol,electron_configuration FROM elements where atomic_number in (%s)",(at_no,))
+        result = cur.fetchall()
+        res = result
+        return render_template('employees.html', result=res, content_type='application/json')
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
