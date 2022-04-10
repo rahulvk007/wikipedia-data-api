48603c0d7e23e1b977b3e1c81891337febed7008
rahulvk007 <59737849+rahulvk007@users.noreply.github.com>
2022-04-10 23:11:18 +0530

    mm

--- index.py
+++ index.py
@@ -4,12 +4,10 @@ import requests
 import sqlite3
 app = Flask(__name__)
 
-
 con = sqlite3.connect('elements.db')
 
-
 @app.route('/', methods=['GET', 'POST'])
-def employees():
+def elements():
     if request.method == "POST":
         details = request.form
         at_no = details['atomic_number']
@@ -28,7 +26,7 @@ def employees():
         data = str()
         for i in x:
             data = data + i.get_text() + '\n'
-        return render_template('employees.html', result=res,dt = data,nam = result[0][0], content_type='application/json')
+        return render_template('elements.html', result=res,dt = data,nam = result[0][0], content_type='application/json')
     return render_template('index.html')
 
 
